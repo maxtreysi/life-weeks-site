d4dc57aa0808ed5c0a7642092b9019d1b4ecdd77
maxtreysi <treysimax@gmail.com>
2025-12-15 13:50:48 +0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40, fontFamily: "system-ui" }}>
+      <h1>Life Weeks</h1>
+      <p>Сайт задеплоен. Дальше сделаем интерактивный календарь.</p>
+    </main>
+  );
+}
